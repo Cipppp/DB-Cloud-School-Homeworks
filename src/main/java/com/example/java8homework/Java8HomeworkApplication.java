5802742f80680cab6af2d5684b8562832b4a70e3
Cipppp <pirvuciprian37@gmail.com>
2022-04-13 22:19:05 +0300

    Project added

--- src/main/java/com/example/java8homework/Java8HomeworkApplication.java
+++ src/main/java/com/example/java8homework/Java8HomeworkApplication.java
@@ -1,13 +1,13 @@
-package com.example.shopifyexposed;
+package com.example.java8homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShopifyExposedApplication {
+public class Java8HomeworkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShopifyExposedApplication.class, args);
+		SpringApplication.run(Java8HomeworkApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/java8homework/Java8HomeworkApplicationTests.java
+++ src/test/java/com/example/java8homework/Java8HomeworkApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.shopifyexposed;
+package com.example.java8homework;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShopifyExposedApplicationTests {
+class Java8HomeworkApplicationTests {
 
 	@Test
 	void contextLoads() {
